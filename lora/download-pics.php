e204d5272ab81ffd80ab0194633323a2a67a5ca0
BLVCK <63218178+adithziva@users.noreply.github.com>
2021-06-30 00:17:47 +0530

    Update download-pics.php

--- lora/download-pics.php
+++ lora/download-pics.php
@@ -4,4 +4,4 @@ $hit_count++;
 @file_put_contents('count.txt', 
 $hit_count); 
 
-header('Location: lorav1.apk'); // redirect to the real file to be downloaded
+header('Location: lora/Lorav1.apk'); // redirect to the real file to be downloaded
